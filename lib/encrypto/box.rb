95c1ea56d51081698ffcb9e582bd57cbd0d33383
Ruben-Hartog <ruben.hartog@gmail.com>
2013-12-05 15:59:32 +0100

    Comment about clone

--- lib/encrypto/box.rb
+++ lib/encrypto/box.rb
@@ -9,6 +9,13 @@ module Encrypto
       @nacl_box.box(value)
     end
 
+    #
+    # The cipher text is cloned. Otherwise the nonce is removed from the cipher
+    # text in memory. It is possible that the same nonce and cipher text
+    # are decrypted multiple times. But NEVER encrypt with the same nonce to
+    # prevent replay attacks. This is prevented by using the RandomNonceBox.
+    # See: https://github.com/cryptosphere/rbnacl/wiki/Secret-Key-Encryption
+    #
     def open(cipher_text)
       @nacl_box.open(cipher_text.clone)
     end
